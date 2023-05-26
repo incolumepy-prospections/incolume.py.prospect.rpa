052a2c92ee57825493b9c73ca889d8311b14dc34
Britodfbr <britodfbr@gmail.com>
2023-05-26 13:48:45 -0300

    refactor: bot > rpa

---
+++ incolume/py/prospect/rpa/reservapr.py
@@ -0,0 +1,34 @@
+import time
+import platform
+import pyautogui
+import logging
+
+
+if 'win' not in platform.system().lower():
+    logging.error(' -- Only Windows --')
+    raise SystemError("This run only Windows!")
+
+
+def automation1(url: str = '') -> None:
+    """Automação para agendamento de sala com pyautogui."""
+    url = url or r'https://intranetsispr2.presidencia.gov.br/' \
+                 r'reservapr/login.php'
+    #
+    pyautogui.alert("Pronto para iniciar a automação?")
+    pyautogui.PAUSE = .5
+
+    pyautogui.press("winleft")
+    time.sleep(2)
+    pyautogui.write("chrome")
+    pyautogui.press("enter")
+
+    pyautogui.hotkey("ctrl", "t")
+    time.sleep(.8)
+
+    pyautogui.write(url)
+    pyautogui.press("enter")
+    pyautogui.confirm('Proceda com o login e pressione OK.')
+
+
+if __name__ == "__main__":  # pragma: no cover
+    automation1()
