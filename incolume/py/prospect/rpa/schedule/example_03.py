fb5a8ae102538347ac7e7e665c32bdf8542b8a2a
Britodfbr <britodfbr@gmail.com>
2023-05-29 17:30:22 -0300

    feat: Exemplo com sintaxe utilizando rocketry.conds.every

---
+++ incolume/py/prospect/rpa/schedule/example_03.py
@@ -0,0 +1,32 @@
+from rocketry import Rocketry
+from rocketry.conds import every
+app = Rocketry()
+
+
+@app.task(every('1sec'))
+def a_cada_segundo():
+    print('a cada segundo')
+
+
+@app.task(every('10seconds'))
+def a_cada_10_segundos():
+    print('a cada 10 segundos')
+
+
+@app.task(every('2s'))
+def a_cada_2_segundos():
+    print('a cada 2 segundos')
+
+
+@app.task(every('1h'))
+def a_cada_hora():
+    print('a cada hora')
+
+
+@app.task(every('2d'))
+def a_cada_2_dias():
+    print('a cada 2 dias')
+
+
+if __name__ == '__main__':  # pragma: no cover
+    app.run()
