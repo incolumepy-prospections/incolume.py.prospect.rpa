a2726260fbbfda462761cd1fe7fb6388985bcaab
Britodfbr <britodfbr@gmail.com>
2023-05-29 17:11:15 -0300

    feat: acrescentado agendamento simples mensais, semanais, diarios ..

--- incolume/py/prospect/rpa/schedule/example_00.py
+++ incolume/py/prospect/rpa/schedule/example_00.py
@@ -2,13 +2,30 @@ from rocketry import Rocketry
 
 app = Rocketry()
 
+
 @app.task('minutely')
 def a_cada_minuto():
     print('minuto a minuto')
 
+
 @app.task('hourly')
 def a_cada_hora():
     print('toda hora')
+
+
+@app.task('daily')
+def a_cada_dia():
+    print('todo dia')
+
+
+@app.task('weekly')
+def a_cada_semana():
+    print('toda semana')
+
+
+@app.task('monthly')
+def a_cada_mes():
+    print('todo mes')
 
 
 if __name__ == '__main__':
