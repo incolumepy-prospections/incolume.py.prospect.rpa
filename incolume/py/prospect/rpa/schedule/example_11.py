75ee7cbe9d9b9d65821f8d1aff267491a4730896
Britodfbr <britodfbr@gmail.com>
2023-05-29 19:25:26 -0300

    feat: Passando retorno entre tarefas

---
+++ incolume/py/prospect/rpa/schedule/example_11.py
@@ -0,0 +1,28 @@
+from rocketry import Rocketry
+from rocketry.args import Return
+from rocketry.conds import every, after_success
+from inspect import stack
+from random import randint
+
+
+app = Rocketry()
+
+
+@app.task(every('3s'))
+def task_01():
+    f"""Run {stack()[0][3]}
+    Return value.
+    """
+    return randint(1, 10)
+
+
+@app.task(after_success(task_01))
+def task_02(value=Return(task_01)):
+    f"""Run {stack()[0][3]}
+    Recebe o parametro da task_01.
+    """
+    print(f'Ran {stack()[0][3]}: task_01 return {value:02} with success.')
+
+
+if __name__ == '__main__':  # pragma: no cover
+    app.run()
