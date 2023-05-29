1f0845039a4c9e6c2ca1c08b5e3e50c3d9783c79
Britodfbr <britodfbr@gmail.com>
2023-05-29 18:35:53 -0300

    feat: controle de processo/thread/async

---
+++ incolume/py/prospect/rpa/schedule/example_05.py
@@ -0,0 +1,41 @@
+from rocketry import Rocketry
+from rocketry.conds import minutely, daily, weekly, monthly
+from inspect import stack
+
+app = Rocketry()
+
+
+@app.task('every 1s', execution='async')
+async def task_01():
+    f"""Run {stack()[0][3]}
+    Executa de modo assincrono.
+    """
+    print(f'Ran {stack()[0][3]}.')
+
+
+@app.task('every 1s', execution='thread')
+def task_02():
+    f"""Run {stack()[0][3]}
+    Executa vinculado ao thread separada.
+    """
+    print(f'Ran {stack()[0][3]}.')
+
+
+@app.task('every 1s', execution='process')
+def task_03():
+    f"""Run {stack()[0][3]}
+    Executa vinculado ao processo separado.
+    """
+    print(f'Ran {stack()[0][3]}.')
+
+
+@app.task('every 1s')
+def task_04():
+    f"""Run {stack()[0][3]}
+    Executa vinculado ao processo/thred principal.
+    """
+    print(f'Ran {stack()[0][3]}.')
+
+
+if __name__ == '__main__':  # pragma: no cover
+    app.run()
