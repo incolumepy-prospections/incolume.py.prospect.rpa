a38f74b919120fc29737ab03da9bd7b2c73fdded
Britodfbr <britodfbr@gmail.com>
2023-05-30 10:25:39 -0300

    feat: tratativa de gui pysimplegui + rocketry

---
+++ incolume/py/prospect/rpa/schedule/example_16.py
@@ -0,0 +1,23 @@
+import datetime
+
+from rocketry import Rocketry
+from rocketry.args import Arg, FuncArg
+from rocketry.conds import every
+from inspect import stack
+from incolume.py.prospect.rpa.gui.screen10 import screen10
+
+
+app = Rocketry()
+app.params(**screen10())
+
+
+@app.task(every('3m'))
+def task_a(username=Arg('username'), password=Arg('password')):
+    f"""Run {stack()[0][3]}
+    Return value.
+    """
+    print(username, password)
+
+
+if __name__ == '__main__':  # pragma: no cover
+    app.run()
