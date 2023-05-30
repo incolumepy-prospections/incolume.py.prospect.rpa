ec1823fc9ddaff3d4217f53ff1864286c7252121
Britodfbr <britodfbr@gmail.com>
2023-05-30 08:49:13 -0300

    feat: Nova implementação com pysimplegui + playwright

--- incolume/py/prospect/rpa/tratativa_reservapr1.py
+++ incolume/py/prospect/rpa/tratativa_reservapr1.py
@@ -1,14 +1,69 @@
+"""Examples RPA with playwright."""
+
+import asyncio
 import time
 import datetime as dt
-import logging
+from playwright.async_api import async_playwright
 from playwright.sync_api import sync_playwright
-from incolume.py.prospect.rpa.gui.screen10 import screen10
+from incolume.py.prospect.rpa.gui.screen7 import screen7
+from incolume.py.prospect.rpa.gui.screen8 import screen8
 
 
-def automation(date: dt.datetime = None,
-               delta: dict[str: int] = None,
-               date_format: str = '%d%m%Y',
-               url: str = "") -> None:
+def sketch01():
+    """Syncrono example."""
+    with sync_playwright() as pw_instance:
+        browser = pw_instance.chromium.launch()
+        page = browser.new_page()
+        page.goto("http://playwright.dev")
+        print(page.title())
+        browser.close()
+
+
+async def sketch02():
+    """Asyncrono example."""
+    async with async_playwright() as pw_instance:
+        browser = await pw_instance.chromium.launch()
+        page = await browser.new_page()
+        await page.goto("http://playwright.dev")
+        print(await page.title())
+        await browser.close()
+
+
+def sketch03():
+    """Screenshot example."""
+    with sync_playwright() as pw_instance:
+        browser = pw_instance.webkit.launch(headless=False, slow_mo=50)
+        page = browser.new_page()
+        page.goto("http://whatsmyuseragent.org/")
+        page.screenshot(path="example.png")
+        browser.close()
+
+
+def automation1(url: str = "") -> None:
+    """Automação para agendamento de sala com playwright."""
+    url = (
+        url
+        or r"https://intranetsispr2.presidencia.gov.br/" r"reservapr/login.php"
+    )
+    info_login = screen7()
+    with sync_playwright() as pw_instance:
+        browser = pw_instance.webkit.launch(headless=False, slow_mo=50)
+        page = browser.new_page()
+        page.goto(url)
+        time.sleep(7)
+        page.screenshot(path="example.png")
+        page.fill('xpath=//*[@id="email-login"]', info_login.get('username'))
+        time.sleep(1)
+        page.locator('xpath=/html/body/div[1]/div[2]/div[2]/button').click()
+        page.fill('xpath=//*[@id="senha"]', info_login.get('password'))
+        time.sleep(5)
+        # browser.close()
+
+
+def automation2(date: dt.datetime = None,
+                delta: dict[str: int] = None,
+                date_format: str = '%d%m%Y',
+                url: str = "") -> None:
     """Automação para agendamento de sala com playwright."""
     delta = delta or {'weeks': 0, 'days': 1}
     date = date or dt.datetime.now() + dt.timedelta(**delta)
@@ -16,8 +71,7 @@ def automation(date: dt.datetime = None,
         url
         or r"https://intranetsispr2.presidencia.gov.br/reservapr/login.php"
     )
-    info_login = screen10()
-    logging.debug(info_login)
+    info_login = screen8()
     with sync_playwright() as pw_instance:
         browser = pw_instance.webkit.launch(headless=False, slow_mo=50)
         page = browser.new_page()
@@ -112,8 +166,16 @@ def automation(date: dt.datetime = None,
 
 
 def run():
-    automation()
+    """Run it.."""
+    # sketch01()
+    # asyncio.run(sketch02())
+    # sketch03()
+    # print(f"{dir()}")
+    # automation1(url="https://portalhashtag.com/login")
+    # automation2()
+    # automation2(delta={'days': 53})
+    automation2(delta={'days': 1})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
